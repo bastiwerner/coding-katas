666c1b38543adaf2245cc19c3091442843e2b674
Bastian Werner <bastian@cyanite.ai>
2024-07-02 13:46:02 +0200

    add fizzbuzz-kata

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # coding-katas
+
+## Requirements
+
+* poetry: `pip3 install poetry`
+
+## Run
+
+```python
+poetry install
+poetry run pytest
+```
---
+++ tests/test_fizz_buzz.py
@@ -0,0 +1,10 @@
+def test_fizzbuzz_returns_fizz_for_multiples_of_three():
+    assert False
+
+
+def test_fizzbuzz_returns_buzz_for_multiples_of_five():
+    assert False
+
+
+def test_fizzbuzz_returns_fizzbuzz_for_multiples_of_three_and_five():
+    assert False
